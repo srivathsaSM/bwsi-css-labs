29f5c4bc00ea8252e805a3277becd651bd0857f8
Srivathsa Maruvada <121247866+srivathsaSM@users.noreply.github.com>
2026-02-20 10:30:19 -0500

    Added Introduction
    Printed the introduction to the course in lab_1a.py below the Welcome message.

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Srivathsa" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hey! My name is Srivathsa, and I am a Junior at Enloe High School in North Carolina. I have a passion for computers, hardware, and programming (proficient in Java). I really enjoy robotics, and I am on FRC Team 2059, The Hitchhikers. I also love playing video games!")
 
 if __name__ == "__main__":
     main()
